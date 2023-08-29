314357f6909d6f4c3cd11bbaf61f0f00a7949a80
Ezra Buehler <ezra.buehler@husqvarnagroup.com>
2023-08-29 20:24:32 +0200

    Add interactive CLI

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
+use cliclack::{confirm, intro, log, outro, select, spinner};
 use core::time::Duration;
 use serialport::{available_ports, SerialPort};
-use std::env;
 use std::io::Read;
 
 fn open_serial_port(path: &str) -> Result<Box<dyn SerialPort>, Box<dyn std::error::Error>> {
@@ -16,7 +16,7 @@ fn remove_non_printable(s: &str) -> String {
 }
 
 fn send(
-    serial_port: Option<&mut Box<dyn SerialPort>>,
+    serial_port: &mut Option<&mut Box<dyn SerialPort>>,
     buf: &[u8],
 ) -> Result<(), Box<dyn std::error::Error>> {
     if let Some(p) = serial_port {
@@ -26,12 +26,13 @@ fn send(
     Ok(())
 }
 
-fn receive(serial_port: Option<&mut Box<dyn SerialPort>>) -> Option<String> {
+fn receive(serial_port: &mut Option<&mut Box<dyn SerialPort>>) -> Option<String> {
     let mut buf: Vec<u8> = vec![0; 1000];
     let bytes_read;
     if let Some(p) = serial_port {
         bytes_read = p.read(buf.as_mut_slice()).unwrap_or(0);
     } else {
+        // TODO: remove stdin support
         bytes_read = std::io::stdin()
             .read_to_end(&mut buf)
             .expect("Failed to read from stdin");
@@ -46,43 +47,20 @@ fn receive(serial_port: Option<&mut Box<dyn SerialPort>>) -> Option<String> {
     Some(s)
 }
 
-fn main() {
+fn analyze(
+    serial_port: &mut Option<&mut Box<dyn SerialPort>>,
+) -> Result<(), Box<dyn std::error::Error>> {
     let mut patterns = vec![
         "U-Boot",
         "DRAM:  128 MiB",
         "Net:   eth0: eth@10110000",
         "=>",
     ];
-
-    let args: Vec<String> = env::args().collect();
-    let serial_port_name = if args.len() > 1 {
-        &args[1]
-    } else {
-        "/dev/ttyUSB0"
-    };
-
-    let mut serial_port: Option<Box<dyn SerialPort>> = None;
-
-    if let Ok(p) = open_serial_port(serial_port_name) {
-        serial_port = Some(p);
-    } else {
-        eprintln!("Could not open serial port {}", serial_port_name);
-        eprintln!("You can pass one of the following as argument:");
-        if let Ok(serial_port_list) = available_ports() {
-            for port in serial_port_list {
-                println!("{}", port.port_name);
-            }
-        } else {
-            eprintln!("Failed to get serial port list");
-        }
-        println!("\nReading from stdin...")
-    }
-
     let mut console_output = String::from("");
     let mut timeout_counter = 0;
 
     loop {
-        if let Some(s) = receive(serial_port.as_mut()) {
+        if let Some(s) = receive(serial_port) {
             console_output += s.as_str();
             timeout_counter = 0;
         } else {
@@ -90,7 +68,7 @@ fn main() {
         }
 
         if let Some(_) = console_output.find(patterns[0]) {
-            println!("{} ✔️", patterns[0]);
+            log::info(format!("{} ✔️", patterns[0]))?;
             patterns.drain(..1);
             if patterns.len() == 0 {
                 break;
@@ -98,7 +76,7 @@ fn main() {
         }
 
         if timeout_counter >= 100 {
-            println!("{} ❌️", patterns[0]);
+            log::info(format!("{} ❌️", patterns[0]))?;
             patterns.drain(..1);
             if patterns.len() == 0 {
                 break;
@@ -106,6 +84,59 @@ fn main() {
             continue;
         }
 
-        send(serial_port.as_mut(), b"x").expect("Failed to write to serial port");
+        send(serial_port, b"x").expect("Failed to write to serial port");
     }
+
+    Ok(())
+}
+
+fn exit(code: i32) -> Result<(), Box<dyn std::error::Error>> {
+    outro("Bye!")?;
+    std::process::exit(code);
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    intro("GARDENA smart gateway boot analyzer")?;
+
+    let mut serial_port_name = String::from("");
+
+    if let Ok(serial_port_list) = available_ports() {
+        if serial_port_list.len() > 0 {
+            let mut sel = select("Select the serial port to use");
+
+            for port in serial_port_list {
+                sel = sel.item(port.port_name.clone(), port.port_name.clone(), "");
+            }
+            serial_port_name = sel.interact()?;
+        } else {
+            log::error("No serial port found")?;
+            exit(1)?;
+        }
+    }
+
+    let mut serial_port: Option<Box<dyn SerialPort>> = None;
+
+    if let Ok(p) = open_serial_port(serial_port_name.as_str()) {
+        serial_port = Some(p);
+    } else {
+        log::error(format!("Could not open serial port {}", serial_port_name))?;
+        exit(1)?;
+    }
+
+    loop {
+        let mut spinner = spinner();
+        spinner.start("");
+
+        analyze(&mut serial_port.as_mut())?;
+
+        spinner.stop("Done");
+
+        if !confirm("Again?").initial_value(true).interact().unwrap() {
+            break;
+        }
+    }
+
+    exit(1)?;
+
+    Ok(())
 }
